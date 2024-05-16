bdfc800baf77ab6e56fe3d9ec244fe7e243dcde6
kwonyul <kw990812@gmail.com>
2024-05-16 14:46:05 +0900

    JavaBean 객체 응답
    json으로 직렬화할때
    get메소드로 프로퍼티가 결정됨

--- src/main/java/com/study/controller/Controller42.java
+++ src/main/java/com/study/controller/Controller42.java
@@ -1,5 +1,6 @@
 package com.study.controller;
 
+import com.study.domain.MyBean411;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -52,5 +53,15 @@ public class Controller42 {
                 "info", Map.of("address", "강남",
                         "country", "한국"),
                 "item", List.of("한옥", "아내"));
+    }
+
+    @GetMapping("sub5")
+    @ResponseBody
+    public MyBean411 method5() {
+        MyBean411 obj = new MyBean411();
+        obj.setName("이강인");
+        obj.setAge(55);
+
+        return obj;
     }
 }
---
+++ src/main/java/com/study/domain/MyBean412.java
@@ -0,0 +1,14 @@
+package com.study.domain;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class MyBean412 {
+    private String name;
+    private Double height;
+    private MyBean411 info;
+    private List<String> foods;
+    private boolean married;
+}
