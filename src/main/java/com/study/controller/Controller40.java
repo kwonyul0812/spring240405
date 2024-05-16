07e99c116eeefb25e02face3f12cae88b65430bd
kwonyul <kw990812@gmail.com>
2024-05-16 10:42:01 +0900

    /api/someurl 의 get,post 요청 핸들러메소드 작성

--- src/main/java/com/study/controller/Controller40.java
+++ src/main/java/com/study/controller/Controller40.java
@@ -3,6 +3,7 @@ package com.study.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class Controller40 {
@@ -25,5 +26,37 @@ public class Controller40 {
     @PostMapping("/api/someurl2")
     public void method4() {
         System.out.println("Controller40.method4");
+    }
+
+    @GetMapping("/api/someurl3")
+    public void method5(@RequestParam("id") Integer id,
+                        @RequestParam("name") String name,
+                        @RequestParam("email") String email) {
+        System.out.println("id = " + id);
+        System.out.println("name = " + name);
+        System.out.println("email = " + email);
+    }
+
+    @PostMapping("/api/someurl3")
+    public void method6(@RequestParam("id") Integer id,
+                        @RequestParam("city") String city,
+                        @RequestParam("country") String country) {
+        System.out.println("id = " + id);
+        System.out.println("city = " + city);
+        System.out.println("country = " + country);
+    }
+
+    @GetMapping("/api/someurl4")
+    public void method7(Integer id, String name, String country) {
+        System.out.println("id = " + id);
+        System.out.println("name = " + name);
+        System.out.println("country = " + country);
+    }
+
+    @PostMapping("/api/someurl4")
+    public void method8(Integer id, String name, String country) {
+        System.out.println("id = " + id);
+        System.out.println("name = " + name);
+        System.out.println("country = " + country);
     }
 }
